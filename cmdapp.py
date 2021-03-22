aa09ea91d60f1a04ab6d76fa25c2ee7919bba5c8
Amy Beisel <amy.beisel@gmail.com>
2021-03-22 11:08:24 -0600

    fixed request

--- cmdapp.py
+++ cmdapp.py
@@ -1,3 +1,11 @@
 import requests
-r = requests.get("http://127.0.0.1:8000/Bikes/")
+r = requests.get("http://localhost:8000/Bike/")
+r1 = requests.get("http://localhost:8000/RearWheel/")
+r2 = requests.get("http://localhost:8000/FrontWheel/")
+
+r = requests.post("http://127.0.0.1:8000/FrontWheel/", json={'bike': {'brand': 'Enve', 'size': '27', 'weight':10}
 print(r.json())
+print(r1.json())
+print(r2.json())
+
+
